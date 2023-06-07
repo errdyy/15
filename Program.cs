1ef1d475f7695d331c922bbda0b3617e1c71a867
errdyy <errdao@gmail.com>
2023-06-07 21:39:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 2, 4, 5, 4, 6, 7, 6 };
+            PrintArray(array);
+
+            bool flag;
+            do
+            {
+                flag = false;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int count = array.Count(x => x == array[i]);//понравилось очень сильно
+                    if (count > 1)
+                    {
+                        array[i] *= i;
+                        flag = true;
+                        PrintArray(array);
+                    }
+                }
+            } while (flag);
+
+
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " ");
+            Console.WriteLine();
+
+        }
+    }
+}
